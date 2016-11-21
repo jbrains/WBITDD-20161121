187ab6ee518bb9aa6383b40276893b42b5247d00
J. B. Rainsberger <me@jbrains.ca>
2016-11-21 12:57:48 +0100

    We have moved a few responsibilities to a more appropriate place.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -3,14 +3,16 @@ package ca.jbrains.pos.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display);
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
+            put("12345", "EUR 7.50");
+        }}));
 
         sale.onBarcode("12345");
 
@@ -20,7 +22,9 @@ public class SellOneItemTest {
     @Test
     public void anotherProductFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display);
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
+            put("23456", "EUR 12.95");
+        }}));
 
         sale.onBarcode("23456");
 
@@ -30,7 +34,11 @@ public class SellOneItemTest {
     @Test
     public void productNotFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display);
+        // Don't let 99999 be a key in this lookup table
+        final Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
+            put("12345", "EUR 7.50");
+            put("23456", "EUR 12.95");
+        }}));
 
         sale.onBarcode("99999");
 
@@ -40,7 +48,7 @@ public class SellOneItemTest {
     @Test
     public void emptyBarcode() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale(display);
+        final Sale sale = new Sale(display, new Catalog(Collections.emptyMap()));
 
         sale.onBarcode("");
 
@@ -48,29 +56,27 @@ public class SellOneItemTest {
     }
 
     public static class Sale {
-        private Display display;
+        private final Display display;
+        private final Catalog catalog;
 
-        public Sale(Display display) {
+        public Sale(Display display, Catalog catalog) {
             this.display = display;
+            this.catalog = catalog;
         }
 
         public void onBarcode(String barcode) {
             if ("".equals(barcode)) {
-                display.setText("Scanning error: empty barcode");
+                display.displayScannedEmptyBarcodeMessage();
                 return;
             }
 
-            final Map<String, String> pricesByBarcode = new HashMap<String, String>() {{
-                put("12345", "EUR 7.50");
-                put("23456", "EUR 12.95");
-            }};
-
-            final String priceAsText = pricesByBarcode.get(barcode);
+            final String priceAsText = catalog.findPrice(barcode);
             if (priceAsText != null)
-                display.setText(priceAsText);
+                display.displayPrice(priceAsText);
             else
-                display.setText(String.format("Product not found for %s", barcode));
+                display.displayProductNotFoundMessage(barcode);
         }
+
     }
 
     public static class Display {
@@ -81,8 +87,16 @@ public class SellOneItemTest {
             return text;
         }
 
-        public void setText(String text) {
-            this.text = text;
+        public void displayPrice(String priceAsText) {
+            this.text = priceAsText;
+        }
+
+        public void displayProductNotFoundMessage(String barcode) {
+            this.text = String.format("Product not found for %s", barcode);
+        }
+
+        public void displayScannedEmptyBarcodeMessage() {
+            this.text = "Scanning error: empty barcode";
         }
     }
 }
