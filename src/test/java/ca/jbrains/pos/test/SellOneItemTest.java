b2ebd76154d30e6124e771b36ccb6559136debbf
J. B. Rainsberger <me@jbrains.ca>
2016-11-21 11:48:33 +0100

    We are now ready to choose different prices for different barcodes.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,53 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
-        final Sale sale = new Sale();
         final Display display = new Display();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("EUR 7.50", display.getText());
     }
 
+    @Ignore("Work in process")
+    @Test
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("EUR 12.95", display.getText());
+    }
+
     public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            display.setText("EUR 7.50");
         }
     }
 
     public static class Display {
+
+        private String text;
+
         public String getText() {
-            return "EUR 7.50";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
