c58759ba49d984ca7c26fe6dd30bfda2c60f662d
J. B. Rainsberger <me@jbrains.ca>
2016-11-22 10:51:14 +0100

    We now avoid processing empty commands.

--- src/test/java/ca/jbrains/pos/test/ConnectToBarcodeScannerTest.java
+++ src/test/java/ca/jbrains/pos/test/ConnectToBarcodeScannerTest.java
@@ -7,7 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.Arrays;
@@ -62,12 +61,40 @@ public class ConnectToBarcodeScannerTest {
         ));
     }
 
+    @Test
+    public void rejectEmptyCommands() throws Exception {
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode("::barcode 1::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 2::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 3::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 4::");
+        }});
+
+        consumeListOfTextCommands(Arrays.asList(
+                "",
+                "",
+                "",
+                "::barcode 1::",
+                "",
+                "::barcode 2::",
+                "",
+                "",
+                "::barcode 3::",
+                "",
+                "::barcode 4::",
+                "",
+                ""
+        ));
+    }
+
     private void consumeListOfTextCommands(List<String> textCommands) {
         consumeTextCommand(new StringReader(unlines(textCommands)));
     }
 
     private void consumeTextCommand(Reader commandSource) {
-        new BufferedReader(commandSource).lines().forEach(barcodeScannedListener::onBarcode);
+        new BufferedReader(commandSource).lines()
+                .filter((line) -> !line.isEmpty())
+                .forEach(barcodeScannedListener::onBarcode);
     }
 
     public interface BarcodeScannedListener {
