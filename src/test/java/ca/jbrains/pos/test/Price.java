6c9e75573c52c6979123690d1f0ea0abb9ea51f0
J. B. Rainsberger <me@jbrains.ca>
2016-11-22 12:53:57 +0100

    We believe that we can display any price to a writer.

--- src/test/java/ca/jbrains/pos/test/Price.java
+++ src/test/java/ca/jbrains/pos/test/Price.java
@@ -1,12 +1,21 @@
 package ca.jbrains.pos.test;
 
 public class Price {
-    public static Price cents(int centsValue) {
-        return new Price();
+    private final int centsValue;
+
+    public Price(int centsValue) {
+        this.centsValue = centsValue;
     }
 
+    public static Price cents(int centsValue) {
+        return new Price(centsValue);
+    }
+
+    public double euro() {
+        return centsValue / 100.0d;
+    }
     @Override
     public String toString() {
-        return "a Price";
+        return String.format("a Price with %d cents", centsValue);
     }
 }
