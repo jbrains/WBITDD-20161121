133b5a07482e7fcd6fe1e08fb31af740e8572296
J. B. Rainsberger <me@jbrains.ca>
2016-11-22 12:41:51 +0100

    Improved tests by weakening assumptions.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -4,18 +4,27 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FindPriceInMemoryCatalogTest extends FindPriceInCatalogContract {
 
     @Override
     protected Catalog catalogWith(String barcode, Price matchingPrice) {
-        return new InMemoryCatalog(Collections.singletonMap(barcode, matchingPrice));
+        return new InMemoryCatalog(new HashMap<String, Price>() {{
+            put(String.format("Not %s", barcode), Price.cents(12));
+            put(barcode, matchingPrice);
+            put(String.format("Definitely not %s", barcode), Price.cents(24));
+        }});
     }
 
     @Override
     protected Catalog catalogWithout(String barcodeToAvoid) {
-        return new InMemoryCatalog(Collections.emptyMap());
+        return new InMemoryCatalog(new HashMap<String, Price>() {{
+            put(String.format("Not %s", barcodeToAvoid), Price.cents(12));
+            put(String.format("Definitely not %s", barcodeToAvoid), Price.cents(24));
+            put(String.format("Certainly not %s", barcodeToAvoid), Price.cents(2489));
+        }});
     }
 
     public static class InMemoryCatalog implements Catalog {
