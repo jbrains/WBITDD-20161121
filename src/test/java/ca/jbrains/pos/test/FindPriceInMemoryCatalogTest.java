93dc15979a4dd85b10c0d5befc53057b6d7cf751
J. B. Rainsberger <me@jbrains.ca>
2016-11-22 12:40:04 +0100

    Extracted contract semantics tests for finding a price in a Catalog.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -6,25 +6,15 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.Map;
 
-public class FindPriceInMemoryCatalogTest {
-    @Test
-    public void productFound() throws Exception {
-        final Price matchingPrice = Price.cents(795);
-        Assert.assertEquals(
-                matchingPrice,
-                catalogWith("12345", matchingPrice).findPrice("12345"));
-    }
+public class FindPriceInMemoryCatalogTest extends FindPriceInCatalogContract {
 
-    private Catalog catalogWith(String barcode, Price matchingPrice) {
+    @Override
+    protected Catalog catalogWith(String barcode, Price matchingPrice) {
         return new InMemoryCatalog(Collections.singletonMap(barcode, matchingPrice));
     }
 
-    @Test
-    public void productNotFound() throws Exception {
-        Assert.assertEquals(null, catalogWithout("12345").findPrice("12345"));
-    }
-
-    private Catalog catalogWithout(String barcodeToAvoid) {
+    @Override
+    protected Catalog catalogWithout(String barcodeToAvoid) {
         return new InMemoryCatalog(Collections.emptyMap());
     }
 
